5b847b249089405e35eb6f9f2e57d4c6bedef699
Greg Allen <greg@firstandthird.com>
2016-11-30 20:01:12 -0800

    updates to prompt, allow for passing in via env vars

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -3,15 +3,21 @@ prompt() {
   local default=$2
   local var=$3
 
-  echo "$question [$default]:"
-  read answer
-  if [[ -z "$answer" ]]; then
-    if [[ -z "$default" ]]; then
-      echo "must set value"
-      exit 1
-    fi
-    eval "$var=$default"
+  if [[ -n "${!var}" ]]; then
+    echo "$question: ${!var}"
   else
-    eval "$var=$answer"
+    echo "$question [${var}=${default}]:"
+    read answer
+    if [[ -z "$answer" ]]; then
+      if [[ -z "$default" ]]; then
+        echo "must set value"
+        exit 1
+      fi
+      eval "$var=$default"
+    else
+      eval "$var=$answer"
+    fi
   fi
+
+
 }
